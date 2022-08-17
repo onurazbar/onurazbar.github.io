a7936e4b5c9b1396f733ff27391048b029c0ac2c
onurazbar <onurazbar@gmail.com>
2022-08-18 00:28:05 +0300

    Added javascript code and initial version of project sites.

---
+++ js/project.js
@@ -0,0 +1,15 @@
+function onSpaceCraftButtonClick() {
+    document.getElementById("project_iframe").setAttribute("src", "spacecraft.html");
+}
+
+function onSnakeButtonClick() {
+    document.getElementById("project_iframe").setAttribute("src", "snake.html");
+}
+
+function onBackGammonButtonClick() {
+    document.getElementById("project_iframe").setAttribute("src", "backgammon.html");
+}
+
+function onRayTracerButtonClick() {
+    document.getElementById("project_iframe").setAttribute("src", "raytracer.html");
+}
