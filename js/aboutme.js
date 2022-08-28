5b0caade937ff854f984893682c8936ff3f9d16c
onurazbar <onurazbar@gmail.com>
2022-08-28 23:09:44 +0300

    Updated link.

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,9 +1,9 @@
 function onDownloadMyCvButtonClick()
 {
-    var cv = document.createElement("cv");
-    cv.href = "../aboutme/Onur_Azbar_Resume.pdf";
-    cv.setAttribute("download", "Onur_Azbar_Resume");
-    cv.click();
+    var a = document.createElement("a");
+    a.href = "Onur_Azbar_Resume.pdf";
+    a.setAttribute("download", "Onur_Azbar_Resume");
+    a.click();
 }
 
 function onBackToHomepageButtonClick()
